7d60632c7d3c9cf35bec678264c97e6fc9840a25
AndreaGiorgione <anadndreagiorgione98@gmail.com>
2022-11-14 18:54:34 +0100

    Restyling of neurons classes

--- topology.py
+++ topology.py
@@ -10,78 +10,109 @@ class Unit:
     input or output unit 
     """
     def __init__(self, activation_function : function,
-    weights_array : np.ndarray, eta : float):
+    weights_array : np.ndarray, bias : float, eta : float):
         """ Defining the constructor.
+
+        Attributes
+        ----------
+        activation:function : function
+            Activation function to be applied to net
+            the current unit, so it is a one variable
+            function defined outside the network.
+
+        weights_array : arraylike of shape (n_components)
+            Weights fot the current unit, taken from the
+            weights matrix defined in the corrisponding layer.
+
+        bias : float
+            Treshold of the output unit.
+
+        eta : float
+            Learning rate for the alghoritm speed control.
         """
         self.activation_function = activation_function
-        self. weights_array = weights_array
+        self.weights_array = weights_array
+        self.bias = bias
         self.eta = eta
 
     def feedforward_unit(self, inputs_ : np.ndarray):
-        """ Computing the output value
+        """ Method for the forward propagation of the unit.
+
+        Arguments
+        ----------
+        inputs_ : arraylike of shape (n_components)
+            Set of data (output) from the units of the previous
+            layer. The lenght of this vector is the fan in.
+
+        Return
+        ----------
+        self.unit_output : float
+            Coputation of the unit output.
         """
         self.inputs_ = inputs_
 
         # computing the net and the output of the unit
-        net = np.inner(self.weights_array, self.inputs_)
-        unit_output = self.activation_function(net)
+        self.net = np.inner(self.weights_array, self.inputs_)
+        self.unit_output = self.activation_function(self.net)
 
-        return [float(net), float(unit_output)]
+        return self.unit_output
 
 
 
 class OutputUnit(Unit):
-    """ Defining the class of the Output Unit
+    """ Defining the class of the output unit.
     """
 
-    def __init__(self, target_ : float, output_prevlayer : np.ndarray):
+    def __init__(self):
+
         # call the contructor of the Father class
         Unit.__init__()
 
-        self.target_ = target_
-        self.output_prevlayer = output_prevlayer
 
+    def backprop_unit(self, target: float):
+        """ Method for the backpropagation of the output unit.
 
-    def backprop_unit(self):
-        
+        Arguments
+        ----------
+        error : arraylike of shape (n_components)
+            Difference between output and label of the output unit.
+
+        fix_eta : bool
+            Control parameter for eta, where False means change.
+
+        Return
+        ----------
+        self.delta : float
+            Small delta for the unit (delta_k).
+        """
         # compute the error signal for the output unit
-        net = mg.tensor(self.feedforward_unit[0])
-        activationf = self.activation_function(net)
-        delta = (self.target_ - self.feedforward_unit[1])*activationf.backward()
+        activationf = self.activation_function(mg.tensor(self.net))
+        delta = (target - self.unit_output) * activationf.backward()
 
         # update the weights
-        for i in enumerate(self.weights_array):
-            self.weights_array[i] = (self.weights_array[i] 
-            + self.eta * delta * self.output_prevlayer[i])
+        self.weights = self.weights + self.eta *  delta * self.inputs_
 
-        return float(delta)
+        return delta
 
 
 
 class HiddenUnit(Unit):
-    """ Defining the class of a Hidden Unit
+    """ Defining the class of a hidden Unit.
     """
 
-    def __init__(self, delta_output : np.ndarray, output_prevlayer : np.ndarray):
+    def __init__(self):
         # call the contructor of the Father class
         Unit.__init__()
 
-        self.delta_output = delta_output
-        self.output_prevlayer = output_prevlayer
-
-    def backprop_unit(self):
-        
+    def backprop_unit(self, delta_next, weights_array_next):
         # compute the error signal for the hidden unit
-        net = mg.tensor(self.feedforward_unit[0])
-        activationf = self.activation_function(net)
-        delta = (np.inner(self.delta_output, self.weights_array)) * activationf.backward()
+        activationf = self.activation_function(mg.tensor(self.net))
+        delta = (np.inner(delta_next, weights_array_next)) * activationf.backward()
 
         # update the weights
-        for i in enumerate(self.weights_array):
-            self.weights_array[i] = (self.weights_array[i] 
-            + self.eta * delta * self.output_prevlayer[i])
+        self.weights_array = self.weights_array + self.eta * delta * self.inputs_
 
-        return float(delta)
+        return delta
 
 
 class Layer:
